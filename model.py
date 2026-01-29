8c5d7b3c08c6848faaa4ccd07293c1b6e73483aa
marcosfsousa <80783407+marcosfsousa@users.noreply.github.com>
2026-01-29 22:59:10 +0100

    added hyperparameters for transfer learn model

--- model.py
+++ model.py
@@ -1,53 +1,28 @@
 # model.py
+from keras.applications import VGG16
 from keras.models import Sequential
 from keras.layers import (
-    Dense, Conv2D, Flatten, Dropout,
-    MaxPooling2D, Input, RandomFlip, Activation,
-    RandomTranslation, RandomRotation, RandomZoom,
-    GlobalAveragePooling2D, BatchNormalization)
+    Input, Dense, Dropout,
+    GlobalAveragePooling2D, BatchNormalization
+)
 from config import INPUT_SHAPE, NUM_CLASSES
 
-
 def build_model():
+    backbone = VGG16(
+        weights="imagenet",
+        include_top=False,
+        input_shape=INPUT_SHAPE
+    )
+    backbone.trainable = True  # 🔒 unfreeze backbone
+
     model = Sequential([
         Input(shape=INPUT_SHAPE),
+        backbone,
 
-        # Data Augmentation
-        RandomFlip("horizontal"),
-        RandomRotation(0.1), # Small rotation helps with orientation invariance
-        RandomTranslation(0.1, 0.1), 
-        
-        #VGG-style architecture
-        
-        # Block 1 - Double Conv (32 filters)
-        Conv2D(32, (3, 3), padding='same', activation='relu'),
-        BatchNormalization(),
-        Conv2D(32, (3, 3), padding='same', activation='relu'),
-        BatchNormalization(),
-        MaxPooling2D((2, 2)),
-        Dropout(0.2),
-        
-        # BLOCK 2: Double Conv (64 filters)
-        Conv2D(64, (3, 3), padding='same', activation='relu'),
-        BatchNormalization(),
-        Conv2D(64, (3, 3), padding='same', activation='relu'),
-        BatchNormalization(),
-        MaxPooling2D((2, 2)),
-        Dropout(0.3),
-
-        # BLOCK 3: Double Conv (128 filters)
-        Conv2D(128, (3, 3), padding='same', activation='relu'),
-        BatchNormalization(),
-        Conv2D(128, (3, 3), padding='same', activation='relu'),
-        BatchNormalization(),
-        MaxPooling2D((2, 2)),
-        Dropout(0.4),
-        
-        # Head
         GlobalAveragePooling2D(),
-        Dense(256, activation='relu'),
+        Dense(256, activation="relu"),
         BatchNormalization(),
         Dropout(0.5),
-        Dense(NUM_CLASSES, activation='softmax')
+        Dense(NUM_CLASSES, activation="softmax")
     ])
     return model
--- train.py
+++ train.py
@@ -1,14 +1,11 @@
 # train.py
 import config
 from keras.callbacks import EarlyStopping, LearningRateScheduler
-from keras.optimizers import SGD
+from keras.optimizers import SGD, Adam
 
 
 def compile_model(model):
-    optimizer = SGD(
-        learning_rate=config.LEARNING_RATE,
-        momentum=config.MOMENTUM,
-        nesterov=True)
+    optimizer = Adam(learning_rate=config.LEARNING_RATE)
 
     model.compile(
         optimizer=optimizer,
@@ -16,25 +13,6 @@ def compile_model(model):
         metrics=["accuracy"],
     )
     return model
-
-def warmup_step_decay(epoch):
-    # Warmup: gradually increase
-    if epoch < 5:
-        return 0.002 * (epoch + 1)   # 0.002 → 0.01
-    
-    # Main phase
-    if epoch < 30:
-        return 0.01
-    
-    # Decay phase 1
-    if epoch < 45:
-        return 0.001
-    
-    # Decay phase 2
-    return 0.0001
-
-lr_scheduler = LearningRateScheduler(warmup_step_decay, verbose=1)
-
 
 def train_model(model, x_train, y_train, batch_size, epochs):
     
@@ -50,7 +28,6 @@ def train_model(model, x_train, y_train, batch_size, epochs):
         batch_size=batch_size,
         epochs=epochs,
         validation_split=0.1,
-        callbacks=[early_stopping, lr_scheduler]
     )
     return history
 
